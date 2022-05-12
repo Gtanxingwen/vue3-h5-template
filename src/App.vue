544f1a045ad1e4be23d46494fbea8eb3f91dacaf
tanxingwen <1689947099@qq.com>
2022-05-12 23:56:31 +0800

    feat: add less

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
   }
 </style>
--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,13 +1,27 @@
 <template>
-  <div> header </div>
-  <div>
+  <header> header </header>
+  <main>
     <PageLayout />
-  </div>
-  <div>bottom</div>
+  </main>
+  <footer>footer</footer>
 </template>
 
 <script lang="ts" setup>
   import PageLayout from './page.vue';
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  header {
+    height: 100px;
+    border-bottom: 1px solid #999;
+  }
+
+  main {
+    flex: 1;
+  }
+
+  footer {
+    height: 100px;
+    border-top: 1px solid #999;
+  }
+</style>
