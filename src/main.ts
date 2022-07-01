21751d526d7a5b86d765f4ed76c41e3bc5c53cad
谭兴文 <tanxingwen@sinoiov.com>
2022-07-01 18:15:44 +0800

    feat: 集成windicss

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import 'virtual:windi.css';
 // 注册svg雪碧图
 import 'virtual:svg-icons-register';
+
 import { createApp } from 'vue';
-// 保证normalize.css 在最前面
-import './assets/css/normalize.css';
+// 保证normalize.css 在最前面：windicss引入后就不需要normalize.css
+// import './assets/css/normalize.css';
 import './assets/scss/global.css';
 
 import App from './App.vue';
